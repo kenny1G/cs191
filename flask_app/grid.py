6f48abb532bcbe2428e561dd82ed0482908e0bdf
Kenny Oseleononmen <kenny1g@cs.stanford.edu>
2023-10-30 21:50:41 -0700

    group photos by day

--- flask_app/grid.py
+++ flask_app/grid.py
@@ -32,20 +32,28 @@ def images():
     conn = sqlite3.connect(database_path)
     c = conn.cursor()
 
-    # Execute the query to fetch all photos
-    c.execute("SELECT * FROM photos")
+    # Execute the query to fetch all photos where DateTaken is not null and order them by taken_date
+    c.execute("SELECT * FROM photos WHERE DateTaken IS NOT NULL ORDER BY DateTaken")
     rows = c.fetchall()
 
     # Close the connection
     conn.close()
 
     # Prepare the images data according to the new database definition
+    print(rows[32])
     images = []
+    current_day = None
     for row in rows:
         file_name = row[2]
-        image_width = row[5]
-        image_height = row[6]
-        # images.html expects (name, width, height)
-        images.append((file_name, image_width, image_height))
+        image_width = row[4]
+        image_height = row[5]
+        taken_date = row[6]
+        # Convert SQL date time to just date
+        taken_date = taken_date.split(' ')[0]
+        # images.html expects (day, [(file_name, width, height), ...])
+        if current_day != taken_date:
+            current_day = taken_date
+            images.append((current_day, []))
+        images[-1][1].append((file_name, image_width, image_height))
 
     return render_template("images.html", images=images)
