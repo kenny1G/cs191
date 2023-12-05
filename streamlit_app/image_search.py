413f0c8443312699c468a044756ae0cad54f11d6
Kenny Oseleononmen <kenny1g@cs.stanford.edu>
2023-12-05 13:02:13 -0800

    finale of old events based approach

--- streamlit_app/image_search.py
+++ streamlit_app/image_search.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import sqlite3
+import uuid
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 import streamlit as st
@@ -10,6 +11,7 @@ from sentence_transformers import SentenceTransformer
 import numpy as np
 from dotenv import load_dotenv
 from sql_queries.queries import GET_PHOTO_BY_DATE
+from google_photos import GooglePhotosApi
 
 ROOT_DIRECTORY = os.path.dirname(os.path.abspath(os.curdir))
 FLASK_PATH = "http://127.0.0.1:5000/static/"
@@ -24,7 +26,6 @@ class ImageSearchApp:
         self.load_env_vars()
         self.init_pinecone()
         self.model = SentenceTransformer("clip-ViT-B-32")
-        self.conn = sqlite3.connect(DB_PATH)
         self.selection = None
 
     def load_env_vars(self):
@@ -37,9 +38,19 @@ class ImageSearchApp:
         pinecone.init(api_key=self.api_key, environment="us-west1-gcp-free")
         self.index = pinecone.Index("cs191")
 
-    def get_query_vector(self, text):
+    def get_query_vector_clip(self, text):
         query_emb = self.model.encode([text], show_progress_bar=False)
         return np.ndarray.tolist(query_emb)
+
+    def search_queries(self, query_vector, top_k=10):
+        query_response = self.index.query(
+            namespace="query_embeddings",
+            vector=query_vector,
+            top_k=top_k,
+            include_values=False,
+            include_metadata=True,
+        )
+        return query_response.get("matches", [])
 
     def search_images(self, query_vector, top_k=10):
         query_response = self.index.query(
@@ -61,34 +72,50 @@ class ImageSearchApp:
         return date_counts
 
     def generate_results(self, date_counts):
-        results = []
-        cursor = self.conn.cursor()
-        for date, score in date_counts:
-            cursor.execute(f"{GET_PHOTO_BY_DATE}", (date,))
-            photos = cursor.fetchall()
-            img_string = ""
-            for photo in photos:
-                photo_path = os.path.join(FLASK_PATH, "converted_photos", photo[0])
-                img_string += f"""
-                <img src="{photo_path}"
-                    style="margin: 5px; height: 300px; object-fit: contain"
-                    alt="Photo taken on {date}" >
+        with sqlite3.connect(DB_PATH) as conn:
+            results = []
+            cursor = conn.cursor()
+            for date, score in date_counts:
+                cursor.execute(f"{GET_PHOTO_BY_DATE}", (date,))
+                photos = cursor.fetchall()
+                img_string = ""
+                for photo in photos:
+                    photo_path = os.path.join(FLASK_PATH, "converted_photos", photo[0])
+                    img_string += f"""
+                    <img src="{photo_path}"
+                        style="margin: 5px; height: 300px; object-fit: contain"
+                        alt="Photo taken on {date}" >
+                    """
+                html_string = f"""
+                <main style='width: 100%;'>
+                    <div style='display: flex; justify-content: center; overflow-x: auto;'>
+                        {img_string}
+                    </div>
+                </main>
                 """
-            html_string = f"""
-            <main >
-            <div style='display: flex; justify-content: center; overflow-x: auto;'>
-            {img_string}
-            </div>
-            </main>
-            """
-            results.append((date, html_string))
-        return results
+                results.append((date, html_string))
+            return results
+
+    def first_pass_search(self, query):
+        query_vector = self.get_query_vector_clip(query)
+        query_matches = self.search_queries(query_vector)
+        if not query_matches:
+            return None
+        # Filter matches with a score less than 0.9
+        query_matches = [match for match in query_matches if match.score >= 0.9]
+        return query_matches
 
     def search_and_display(self, query):
-        query_vector = self.get_query_vector(query)
-        matches = self.search_images(query_vector)
-        date_counts = self.aggregate_results(matches)
-        return self.generate_results(date_counts)
+        first_pass = self.first_pass_search(query)
+        st.write(first_pass)
+        if not first_pass:
+            query_vector = self.get_query_vector_clip(query)
+            matches = self.search_images(query_vector)
+            date_counts = self.aggregate_results(matches)
+            return (self.generate_results(date_counts), query_vector)
+        else:
+            date_counts = self.aggregate_results(first_pass)
+            return (self.generate_results(date_counts), None)
 
 
 if "app" not in st.session_state:
@@ -107,26 +134,41 @@ def click_search_button():
     st.session_state["app_state"] = RESULTS
 
 
-def click_date_button(date):
+# We update pinecone with queries that users have matched with an event
+def click_date_button(date, query_vector, query):
     st.session_state["app_state"] = LEARNINGS
+    query_id = uuid.uuid4()
+    app.index.upsert(
+        vectors=[
+            {
+                "id": str(query_id),
+                "values": query_vector[0],
+                "metadata": {"date": date, "query": query},
+            }
+        ],
+        namespace="query_embeddings",
+    )
     app.selection = date
 
 
 st.button("Search", on_click=click_search_button)
 
-st.write(st.session_state["app_state"])
 match st.session_state["app_state"]:
     case 0:
-        st.write(f"You selected {app.selection}")
+        st.header("Learnings")
     case 1:
         try:
-            results = app.search_and_display(query)
+            results, query_vector = app.search_and_display(query)
             for date, html_string in results:
-                st.button(date, on_click=click_date_button, args=[date])
+                if query_vector:
+                    st.button(
+                        date,
+                        on_click=click_date_button,
+                        args=[date, query_vector, query],
+                    )
                 components.html(html_string, height=400)
         except Exception as e:
             st.write("An error occurred during the search.")
             st.write(str(e))
-        st.write("Results")
     case _:
         st.write(st.session_state)
