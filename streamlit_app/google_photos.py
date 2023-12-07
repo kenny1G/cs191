581e6d0a0963d6c8df825a3b756c04b5abf08574
Kenny Oseleononmen <kenny1g@cs.stanford.edu>
2023-12-06 21:58:59 -0800

    self onboarding

--- streamlit_app/google_photos.py
+++ streamlit_app/google_photos.py
@@ -10,11 +10,15 @@ import pandas as pd
 from datetime import date, timedelta, datetime
 import requests
 from PIL import Image
+import pinecone
+import random
+import itertools
 
 
 class GooglePhotosApi:
     def __init__(
         self,
+        email="oseleonose@gmail.com",
         api_name="photoslibrary",
         client_secret_file=r"./credentials/client_secret.json",
         api_version="v1",
@@ -31,14 +35,12 @@ class GooglePhotosApi:
             service:
         """
 
+        self.email = email
         self.api_name = api_name
         self.client_secret_file = client_secret_file
         self.api_version = api_version
         self.scopes = scopes
-        self.cred_pickle_file = (
-            f"./credentials/token_{self.api_name}_{self.api_version}.pickle"
-        )
-
+        self.cred_pickle_file = f"./credentials/token_{self.api_name}_{self.api_version}_{self.email}.pickle"
         self.cred = None
 
     def run_local_server(self):
@@ -60,7 +62,6 @@ class GooglePhotosApi:
             with open(self.cred_pickle_file, "wb") as token:
                 pickle.dump(self.cred, token)
 
-
     # Populate class' media_items_df with Photos in the date range specified
     #
     # Fields of note in the data frame:
@@ -68,7 +69,9 @@ class GooglePhotosApi:
     # **baseUrl** Base URLs within the Google Photos Library API allow
     #  you to access the bytes of the media items. They are valid for 60 minutes.
     # (https://developers.google.com/photos/library/guides/access-media-items)
-    def get_media_files(self, img_model, sdate=date(2023, 2, 1), edate=date(2023, 4, 1)):
+    def get_embed_and_upsert_photos(
+        self, img_model, index_name, sdate=date(2023, 2, 1), edate=date(2023, 4, 1)
+    ):
         # create a list with all dates between start date and end date
         # edate = date.today()
         date_list = pd.date_range(sdate, edate - timedelta(days=1), freq="d")
@@ -76,22 +79,32 @@ class GooglePhotosApi:
         self.media_items_df = pd.DataFrame()
 
         self.media_items_pickle_file = (
-            f"./data/media_items_{sdate}_to_{edate}.pickle"
+            f"./data/media_items_{self.email}.pickle"
         )
         if os.path.exists(self.media_items_pickle_file):
             with open(self.media_items_pickle_file, "rb") as items_df:
                 self.media_items_df = pickle.load(items_df)
             print(f"{len(self.media_items_df)} images sourced from pickle file")
         else:
+            print(f"google_photos.py:: Fetching images from Google Photos API")
             for date in date_list:
                 # get a list with all media items for specified date (year, month, day)
                 items_df, self.media_items_df = self.list_of_media_items(
                     year=date.year,
                     month=date.month,
-                    day=date.day
+                    day=date.day,
+                    media_items_df=self.media_items_df,
                 )
+            num_images = len(self.media_items_df)
+            print(f"google_photos.py:: {num_images} images captured")
+            if num_images == 0:
+                return False
 
-            self.media_items_df.drop(["productUrl", "mimeType", "filename"], axis=1, inplace=True)
+            # Build metadata in the data frame
+            print(f"google_photos.py:: Building metadata in the data frame")
+            self.media_items_df.drop(
+                ["productUrl", "mimeType", "filename"], axis=1, inplace=True
+            )
             self.media_items_df["year"] = [
                 int(x) for x in self.media_items_df["creationTime"].str[0:4].values
             ]
@@ -103,10 +116,13 @@ class GooglePhotosApi:
             ]
             self.media_items_df.reset_index(drop=True, inplace=True)
 
-            url_list = self.media_items_df['baseUrl'].values.tolist()
+            # Load images into memory so we can embed
+            print(f"google_photos.py:: Loading images into memory")
+            url_list = self.media_items_df["baseUrl"].values.tolist()
             images = []
             errored = []
-            images_pickle_file = f"./data/images_{sdate}_to_{edate}.pickle"
+            images_pickle_file = f"./data/images_{self.email}.pickle"
+
             if os.path.exists(images_pickle_file):
                 with open(images_pickle_file, "rb") as images_file:
                     images = pickle.load(images_file)
@@ -116,27 +132,39 @@ class GooglePhotosApi:
                     try:
                         images.append(Image.open(requests.get(i, stream=True).raw))
                     except:
-                        images.append(Image.open(requests.get(url_list[0], stream=True).raw))
+                        images.append(
+                            Image.open(requests.get(url_list[0], stream=True).raw)
+                        )
                         errored.append(i)
-                        print('Image error')
+                        print("Image error")
+
                 with open(images_pickle_file, "wb") as images_file:
                     pickle.dump(images, images_file)
 
+            # Embed images
+            print(f"google_photos.py:: Embedding images")
             embeddings = img_model.encode(images)
             img_embeddings = []
             for i, embedding in enumerate(embeddings):
                 img_embeddings.append(embedding.tolist())
 
-            self.media_items_df['vector'] = img_embeddings
-            self.media_items_df['metadata'] = self.media_items_df.loc[:,['year','month','day']].to_dict('records')
-            self.media_items_df.head()
+            self.media_items_df["vector"] = img_embeddings
+            self.media_items_df["metadata"] = self.media_items_df.loc[
+                :, ["year", "month", "day"]
+            ].to_dict("records")
+            print(f"google_photos.py:: {self.media_items_df.head()}")
+
+            self.upsert_to_pinecone(index_name)
 
             with open(self.media_items_pickle_file, "wb") as items_df:
                 pickle.dump(self.media_items_df, items_df)
 
-            print(f"{len(self.media_items_df)} images fetched from Google Photos API")
+            print(
+                f"google_photos.py:: {len(self.media_items_df)} images fetched from Google Photos API"
+            )
+        return True
 
-    def list_of_media_items(self, year, month, day):
+    def list_of_media_items(self, year, month, day, media_items_df):
         """
         Args:
             year, month, day: day for the filter of the API call
@@ -160,12 +188,12 @@ class GooglePhotosApi:
 
                 # append the existing media_items data frame
                 items_list_df = pd.concat([items_list_df, items_df])
-                self.media_items_df = pd.concat([self.media_items_df, items_df])
+                media_items_df = pd.concat([media_items_df, items_df])
 
         except:
             print(response.text)
 
-        return (items_list_df, self.media_items_df)
+        return (items_list_df, media_items_df)
 
     def get_response_from_medium_api(self, year, month, day):
         url = "https://photoslibrary.googleapis.com/v1/mediaItems:search"
@@ -180,30 +208,65 @@ class GooglePhotosApi:
         }
 
         try:
-            res = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+            res = requests.request(
+                "POST", url, data=json.dumps(payload), headers=headers
+            )
         except:
             print("Request error")
 
         return res
 
+    def upsert_to_pinecone(self, index_name):
+        print(f"google_photos.py:: Upserting to Pinecone")
+        # vector dimenstions
+        vdim = len(self.media_items_df["vector"][0])
+        print(f"{vdim} dimentions in each vector")
 
+        vectors = zip(
+            self.media_items_df.id,
+            self.media_items_df.vector,
+            self.media_items_df.metadata,
+        )
 
+        # Upsert data with 100 vectors per upsert request asynchronously
+        # - Create pinecone.Index with pool_threads=30 (limits to 30 simultaneous requests)
+        # - Pass async_req=True to index.upsert()
+        with pinecone.Index(index_name=index_name, pool_threads=30) as index:
+            # Send requests in parallel
+            async_results = [
+                index.upsert(
+                    vectors=ids_vectors_chunk, namespace=self.email, async_req=True
+                )
+                for ids_vectors_chunk in chunks(vectors, batch_size=100)
+            ]
+            # Wait for and retrieve responses (this raises in case of error)
+            [async_result.get() for async_result in async_results]
 
+        while index.describe_index_stats()["total_vector_count"] == 0:
+            print(index.describe_index_stats())
 
     # Load images into memory, returns list of images loaded
     def load_images_in_mem(self):
-
-        url_list = self.media_items_df['baseUrl'].values.tolist()
+        url_list = self.media_items_df["baseUrl"].values.tolist()
         self.images = []
         errored = []
         for i in url_list:
             try:
                 self.images.append(Image.open(requests.get(i, stream=True).raw))
             except:
-                self.images.append(Image.open(requests.get(url_list[0], stream=True).raw))
+                self.images.append(
+                    Image.open(requests.get(url_list[0], stream=True).raw)
+                )
                 errored.append(i)
-                print('Image error')
+                print("Image error")
         # print(f"{len(images)} images loaded into memory")
         # print(f"{len(errored)} images failed to load")
-        print(errored)
 
+
+def chunks(iterable, batch_size=100):
+    """A helper function to break an iterable into chunks of size batch_size."""
+    it = iter(iterable)
+    chunk = tuple(itertools.islice(it, batch_size))
+    while chunk:
+        yield chunk
+        chunk = tuple(itertools.islice(it, batch_size))
--- streamlit_app/image_search.py
+++ streamlit_app/image_search.py
@@ -20,24 +20,39 @@ ROOT_DIRECTORY = os.path.dirname(os.path.abspath(os.curdir))
 FLASK_PATH = "http://127.0.0.1:5000/static/"
 INSTANCE_DIR = os.path.join(ROOT_DIRECTORY, "instance")
 DB_PATH = os.path.join(INSTANCE_DIR, "photos.db")
-LEARNINGS = 0
-RESULTS = 1
+AUTH = 0
+DATE_SELECTION = 1
+LEARNINGS = 2
+RESULTS = 3
 
 
 def month_name(month):
-    if month == 1: month_text = 'January'
-    if month == 2: month_text = 'February'
-    if month == 3: month_text = 'March'
-    if month == 4: month_text = 'April'
-    if month == 5: month_text = 'May'
-    if month == 6: month_text = 'June'
-    if month == 7: month_text = 'July'
-    if month == 8: month_text = 'August'
-    if month == 9: month_text = 'September'
-    if month == 10: month_text = 'October'
-    if month == 11: month_text = 'November'
-    if month == 12: month_text = 'December'
+    if month == 1:
+        month_text = "January"
+    if month == 2:
+        month_text = "February"
+    if month == 3:
+        month_text = "March"
+    if month == 4:
+        month_text = "April"
+    if month == 5:
+        month_text = "May"
+    if month == 6:
+        month_text = "June"
+    if month == 7:
+        month_text = "July"
+    if month == 8:
+        month_text = "August"
+    if month == 9:
+        month_text = "September"
+    if month == 10:
+        month_text = "October"
+    if month == 11:
+        month_text = "November"
+    if month == 12:
+        month_text = "December"
     return month_text
+
 
 class ImageSearchApp:
     def __init__(self):
@@ -47,9 +62,9 @@ class ImageSearchApp:
         self.new_index = pinecone.Index(self.index_name)
         # self.text_model = SentenceTransformer('clip-ViT-B-32')
         self.model = SentenceTransformer("clip-ViT-B-32")
-        self.google_photos_api = GooglePhotosApi()
-        self.google_photos_api.run_local_server()
-        self.google_photos_api.get_media_files(self.model)
+        # self.google_photos_api = GooglePhotosApi()
+        # self.google_photos_api.run_local_server()
+        # self.google_photos_api.get_media_files(self.model)
         self.selection = None
 
     def load_env_vars(self):
@@ -59,17 +74,16 @@ class ImageSearchApp:
             print("Warning: No Pinecone API key found.")
 
     def init_pinecone(self):
-        pinecone.init(api_key=self.api_key, environment="us-west1-gcp")
+        pinecone.init(api_key=self.api_key, environment="us-west1-gcp-free")
         self.index = pinecone.Index("cs191")
 
     def get_images(self):
-        return self.google_photos_api.media_items_df['baseUrl']
-
+        return self.google_photos_api.media_items_df["baseUrl"]
 
     def search_and_display(self, _query):
         years_filter = [2023]
         months_filter = [2]
-        top_k = 10
+        top_k = 30
         return self.query_images(_query, years_filter, months_filter, top_k)
         # first_pass = self.first_pass_search(query)
         # st.write(first_pass)
@@ -83,30 +97,40 @@ class ImageSearchApp:
         #     return (self.generate_results(date_counts), None)
 
     def query_images(self, query, years_filter, months_filter, top_k):
-
         # create the query vector
         xq = self.model.encode(query).tolist()
 
         # now query
-        xc = self.new_index.query(xq,
-                        filter= {
-                            "year": {"$in":years_filter},
-                            "month": {"$in":months_filter}
-                        },
-                        top_k= top_k,
-                        include_metadata=True)
+        xc = self.new_index.query(
+            xq,
+            namespace=self.namespace,
+            # filter={"year": {"$in": years_filter}, "month": {"$in": months_filter}},
+            top_k=top_k,
+            include_metadata=True,
+        )
 
         img_urls = []
         meta_text = []
 
         media_items_df = self.google_photos_api.media_items_df
-        for i in range(0,top_k):
-            img_id = xc['matches'][i]['id']
-            img_url = media_items_df.loc[media_items_df['id'] == img_id, 'baseUrl'].iloc[0]
+        print(xc)
+        for i in range(0, top_k):
+            img_id = xc["matches"][i]["id"]
+            # print("got id", img_id)
+            img_url = media_items_df.loc[
+                media_items_df["id"] == img_id, "baseUrl"
+            ].iloc[0]
+            # print("got url", img_url)
             img_urls.append(img_url)
-            img_year = media_items_df.loc[media_items_df['id'] == img_id, 'metadata'].iloc[0]['year']
-            img_month = media_items_df.loc[media_items_df['id'] == img_id, 'metadata'].iloc[0]['month']
-            img_text = month_name(img_month) + ' of ' + str(img_year)
+            img_year = media_items_df.loc[
+                media_items_df["id"] == img_id, "metadata"
+            ].iloc[0]["year"]
+            # print("got year", img_year)
+            img_month = media_items_df.loc[
+                media_items_df["id"] == img_id, "metadata"
+            ].iloc[0]["month"]
+            # print("got month", img_month)
+            img_text = month_name(img_month) + " of " + str(img_year)
             meta_text.append(img_text)
 
         # ipyplot.plot_images(img_urls, meta_text, img_width=250, show_url=False)
@@ -182,27 +206,23 @@ class ImageSearchApp:
             return results
 
 
-
-
 st.set_page_config(layout="wide")
 if "app" not in st.session_state:
     st.session_state.app = ImageSearchApp()
 
 if "app_state" not in st.session_state:
-    st.session_state["app_state"] = LEARNINGS
+    st.session_state["app_state"] = AUTH
 
 if "search_journey" not in st.session_state:
     st.session_state["search_journey"] = []
 
 app = st.session_state.app
 
-st.title("Storylines: Smart Image Search")
-query = st.text_input("Search till you find it!", key="text_input_query")
 
-
-def click_search_button():
-    st.session_state["app_state"] = RESULTS
-    st.session_state.search_journey.append(query)
+def click_search_button(query):
+    if st.session_state["app_state"] != AUTH:
+        st.session_state["app_state"] = RESULTS
+        st.session_state.search_journey.append(query)
     # Clear the text in the search bar
     # query = st.session_state["text_input_query"]
     # st.session_state["text_input_query"] = ""
@@ -225,31 +245,78 @@ def click_date_button(date, query_vector, query):
     app.selection = date
 
 
-st.button("Search", on_click=click_search_button)
+def click_auth_button(email):
+    app.google_photos_api = GooglePhotosApi(email=email)
+    app.google_photos_api.run_local_server()
+    app.namespace = email
+    # app.google_photos_api.get_media_files(app.model)
+    media_items_pickle_file = f"./data/media_items_{email}.pickle"
+    if os.path.exists(media_items_pickle_file):
+        app.google_photos_api.get_embed_and_upsert_photos(app.model, app.index_name)
+        st.session_state["app_state"] = LEARNINGS
+    else:
+        st.session_state["app_state"] = DATE_SELECTION
 
 
+def click_date_range_button(start_date, end_date):
+    # Check if the date range is longer than 3 months
+    if (end_date - start_date).days > 90:
+        st.warning(
+            "The date range is longer than 3 months. Please select a shorter range.",
+            icon="⚠️",
+        )
+    else:
+        if app.google_photos_api.get_embed_and_upsert_photos(
+            app.model, app.index_name, start_date, end_date
+        ):
+            st.session_state["app_state"] = LEARNINGS
+        else:
+            st.warning("No images found for the specified date range.", icon="⚠️")
+
+
+st.title("Storylines: Smart Image Search")
 match st.session_state["app_state"]:
     case 0:
-        #TODO: Cleaner gallery, organized by date
+        st.header("Authentication")
+        st.write(
+            "Please Enter the email address associated with your Google Photos account"
+        )
+        email = st.text_input("Email")
+        st.button("Authenticate", on_click=click_auth_button, args=[email])
+    case 1:
+        st.header("Date Selection")
+        st.write("Please select a date range for your search")
+        start_date = st.date_input("Start Date")
+        end_date = st.date_input("End Date")
+        st.button(
+            "Confirm", on_click=click_date_range_button, args=[start_date, end_date]
+        )
+    case 2:
+        query = st.text_input("Search till you find it!", key="text_input_query")
+        st.button("Search", on_click=click_search_button, args=[query])
+        # TODO: Cleaner gallery, organized by date
         st.header("Gallery")
         image_urls = app.get_images()
         controls = st.columns(3)
         with controls[0]:
-            batch_size = st.select_slider("Batch size:",range(10,110,10))
+            batch_size = st.select_slider("Batch size:", range(10, 110, 10))
         with controls[1]:
-            row_size = st.select_slider("Row size:", range(1,6), value = 5)
-        num_batches = ceil(len(image_urls)/batch_size)
+            row_size = st.select_slider("Row size:", range(1, 6), value=5)
+        num_batches = ceil(len(image_urls) / batch_size)
         with controls[2]:
-            page = st.selectbox("Page", range(1,num_batches+1))
+            page = st.selectbox("Page", range(1, num_batches + 1))
 
-        batch = image_urls[(page-1)*batch_size : page*batch_size]
+        batch = image_urls[(page - 1) * batch_size : page * batch_size]
         grid = st.columns(row_size)
         col = 0
         for image_url in batch:
             with grid[col]:
                 st.image(image_url)
             col = (col + 1) % row_size
-    case 1:
+    case 3:
+        st.header("Results")
+        query = st.text_input("Search till you find it!", key="text_input_query")
+        st.button("Search", on_click=click_search_button, args=[query])
         try:
             # results, query_vector = app.search_and_display(query)
             # for date, html_string, image_paths in results:
@@ -262,14 +329,16 @@ match st.session_state["app_state"]:
             #     components.html(html_string, height=400)
             image_urls = app.search_and_display(query)
             cols = st.columns(len(image_urls))
-            img = image_select(label="Select the image", images=image_urls, return_value='index')
+            img = image_select(
+                label="Select the image", images=image_urls, return_value="index"
+            )
+            # TODO: LEARN!
             print(img)
             # for col, image_path in zip(cols, image_urls):
             #     col.image(image_path, width=300)
             # for i, image_path in enumerate(image_paths):
             #     st.image(image_path, width=300)
             # st.image(image_paths[:10], width=100)
-
 
         except Exception as e:
             st.write("An error occurred during the search.")
@@ -282,4 +351,5 @@ with st.sidebar:
         st.header("Search Journey So Far")
         for user_query in st.session_state.search_journey:
             st.write(user_query)
-
+        if st.button("Clear Search Journey"):
+            st.session_state.search_journey = []
